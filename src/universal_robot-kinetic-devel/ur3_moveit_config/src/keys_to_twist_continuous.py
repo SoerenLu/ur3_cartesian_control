165921124c307d58aafdbf6bd4834104e3bb36a2
Sören Langhorst <langhorst@fzi.de>
2017-09-03 18:51:39 +0200

    Added keyboard as input option

--- src/universal_robot-kinetic-devel/ur3_moveit_config/src/keys_to_twist_continuous.py
+++ src/universal_robot-kinetic-devel/ur3_moveit_config/src/keys_to_twist_continuous.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from sensor_msgs.msg import Joy
+from std_msgs.msg import String, Int8MultiArray
 from geometry_msgs.msg import Twist
 
 #the keymapping is not in use, but I do not dare to delete it for some reason
@@ -10,50 +10,51 @@ key_mapping = { 'q':[1,0,0,0,0,0], 'a':[-1,0,0,0,0,0], 'w':[0,1,0,0,0,0],
 def keys_cb(msg, args):
   #args is a reference to the twist message that is published and it has to be updated according to msg (which is of type Joy)
  #move forward
- if msg.buttons[3] == 1:
+ if msg == 'w':
   args[0].linear.x = 0.005
  #move backward
- elif msg.buttons[1] == 1:
+ elif msg == 's':
   args[0].linear.x = -0.005
  #move left
- if msg.buttons[0] == 1:
+ if msg == 'a':
   args[0].linear.x = 0.005
  #move right
- elif msg.button[2] == 1:
+ elif msg == 'd':
   args[0].linear.x = -0.005
  #move up
- if msg.buttons[5] == 1:
+ if msg == 'r':
   args[0].linear.x = 0.005
  #move down
- elif msg.buttons[7] == 1:
-  args[0].linear.x = -0.005if msg
+ elif msg == 'f':
+  args[0].linear.x = -0.005
  #rotate 1 +
- if msg.axes[6] == -1:
+ if msg == 'o':
   args[0].angular.x = 0.02
  #rotate 1 -
- elif msg.axes[6] == 1:
+ elif msg == 'u':
   args[0].angular.x = -0.02
  #rotate 2 +
- if msg.axes[7] == 1:
+ if msg == 'l':
   args[0].angular.y = 0.02
  #rotate 2 -
- elif msg.axes[7] ==-1:
+ elif msg == 'j':
   args[0].angular.y = -0.02
  #rotate 3 +
- if msg.buttons[4] == 1:
+ if msg == 'u':
   args[0].angular.z = 0.02;
  #rotate 3 -
- elif msg.buttons[6] ==1:
+ elif msg == 'j':
   args[0].angular.z = -0.02;
 
 
 if __name__=='__main__':
  contTwist = [Twist()] #put empty Twist object in a list (which is filled in the callback function)
- rospy.init_node('joy2twist_continuous_node')
+ rospy.init_node('keys2twist_continuous_node')
  joy_pub = rospy.Publisher('twist_continuous', Twist, queue_size=1)
- rospy.Subscriber('joy',Joy, keys_cb, contTwist)
+ rospy.Subscriber('keys',String, keys_cb, contTwist)
  rate = rospy.Rate(20)
  while not rospy.is_shutdown():
   joy_pub.publish(contTwist[0])
   rate.sleep()
  rospy.spin()
+
